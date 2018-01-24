502b27bfec4c65a0962953bb1a9a6d933e39f79d
tskon <beatxd@gmail.com>
2018-01-24 22:42:05 +0300

    news-parser: rewrite news page in index

--- news-parser.js
+++ news-parser.js
@@ -2,34 +2,27 @@ const http = require('http');
 const request = require('request');
 const fs = require('fs');
 
-const startSrv = (data) => {
-  const srv = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end(data);
-  });
-  srv.listen(80);
-  console.log('server has been started');
-};
-
-request('https://news.yandex.ru/computers.html?from=index', (err, res, body) => {
-  if (err) {
-    console.log('error:', err);
-    throw err;
-  }
-  console.log('ya-news statusCode:', res.statusCode);
-  createIndex(body);
-});
-
 const createIndex = (data) => {
   fs.writeFile('./index.html', data, (err) => {
     if (err) throw err;
     console.log('index.html has been wrote');
-    const index = fs.readFile('./index.html', (err) => {
-      if (err) throw err;
-      startSrv(index);
-    })
+
+    http.createServer((req, res) => {
+
+      fs.readFile('./index.html', function (err, file) {
+        if (err) throw err;
+
+        res.writeHead(200);
+        res.write(file);
+        res.end();
+      });
+
+    }).listen(80);
+    console.log('server has been started http://localhost/');
   });
 };
 
-
-
+request('https://news.yandex.ru/computers.html?from=index', (err, res, body) => {
+  if (err) throw err;
+  createIndex(body);
+});
