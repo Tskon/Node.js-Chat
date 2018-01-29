67f5bd3f2cca53616dff48901cfe04c1aad29f26
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-01-30 01:51:49 +0300

    console game: log parser complete!

--- news-parser.js
+++ news-parser.js
@@ -2,33 +2,29 @@ const http = require('http');
 const request = require('request');
 const fs = require('fs');
 const cheerio = require('cheerio');
-let $;
 
 request('https://news.yandex.ru/computers.html?from=index', (err, res, html) => {
-    if (err) throw err;
-    $.load(html);
-    let news = '';
-    news += $.
-    addNews(news, '.container');
+  if (err) throw err;
+  const $ = cheerio.load(html);
+  let news = $('.story');
+  addNews(news, '.container');
 });
 
 
-function addNews (data, wrapSelector){
-  fs.writeFile('./index.html', data, (err) => { //убрать, вместо этого писать в враппер
-    if (err) throw err;
-    console.log('index.html has been wrote');
+function addNews(data, wrapSelector) {
 
-    http.createServer((req, res) => {
+  http.createServer((req, res) => {
 
-      fs.readFile('./index.html', function (err, file) {
-        if (err) throw err;
+    fs.readFile('./index.html', function (err, file) {
+      if (err) throw err;
+      const $ = cheerio.load(file);
+      $(wrapSelector).html(data);
+      res.writeHead(200);
+      res.write($.html());
+      res.end();
+    });
 
-        res.writeHead(200);
-        res.write(file);
-        res.end();
-      });
+  }).listen(80);
+  console.log('server has been started http://localhost/');
 
-    }).listen(80);
-    console.log('server has been started http://localhost/');
-  });
 }
