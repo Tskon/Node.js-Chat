ef0170e28597298c7bbc688186ca5eeadc7a7726
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-01 00:41:57 +0300

    lesson4 app: express + jade

--- lesson4-app/app.js
+++ lesson4-app/app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const jade = require('jade');
+const app = express();
+const PORT = 80;
+
+// app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  let html = jade.compileFile('./lesson4-app/views/index.jade');
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log('server has been started http://localhost:' + PORT)
+});
