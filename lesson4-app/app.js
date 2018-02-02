228e3224cba5560dc7a49bfad48a192e89812185
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-03 00:58:33 +0300

    lesson4-app: async news-parser completed

--- lesson4-app/app.js
+++ lesson4-app/app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const jade = require('jade');
+const cheerio = require('cheerio');
 const app = express();
 const PORT = 80;
 
 // app.use(bodyParser());
+const renderIndex = () => {
+  return jade.renderFile('./lesson4-app/views/index.jade', (err, html) => {
+    if (err) throw err;
+    return html;
+  });
+};
+
+function sendRequest(url, selector, callback) {
+  request(url, (err, res, html) => {
+    if (err) throw err;
+    const $ = cheerio.load(html);
+    let selection = $(selector);
+    callback(selection.html());
+  });
+}
 
 app.get('/', (req, res) => {
-  let html = jade.compileFile('./lesson4-app/views/index.jade');
-  res.send(html);
+  res.send(renderIndex());
+});
+
+app.get('/news-list', (req, res) => {
+  let html = renderIndex();
+  const $ = cheerio.load(html);
+  function yield(data) {
+    $('#container').html(data);
+    res.send($.html());
+  }
+  sendRequest('https://news.yandex.ru/politics.html?from=rubric', '.story', yield);
+
 });
 
 app.listen(PORT, () => {
