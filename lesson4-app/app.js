eeef3ab26505bc6bf18139c133d11515e5c2125b
Konstantin Tsekhmeister <tsekhmeister.k@gmail.com>
2018-02-03 02:07:00 +0300

    lesson4-app: news select is works

--- lesson4-app/app.js
+++ lesson4-app/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const url = require('url');
 const bodyParser = require('body-parser');
 const request = require('request');
 const jade = require('jade');
 const cheerio = require('cheerio');
 const app = express();
 const PORT = 80;
+let urlQuery;
 
-// app.use(bodyParser());
 const renderIndex = () => {
   return jade.renderFile('./lesson4-app/views/index.jade', (err, html) => {
     if (err) throw err;
@@ -15,6 +16,7 @@ const renderIndex = () => {
 };
 
 function sendRequest(url, selector, callback) {
+  console.log(url, selector);
   request(url, (err, res, html) => {
     if (err) throw err;
     const $ = cheerio.load(html);
@@ -23,18 +25,48 @@ function sendRequest(url, selector, callback) {
   });
 }
 
+app.use(bodyParser());
+
+app.use((req, res, next) => {
+  urlQuery = url.parse(req.url, true).query;
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send(renderIndex());
 });
 
 app.get('/news-list', (req, res) => {
+  console.log(urlQuery);
+  let newsTheme;
+  let siteObj;
+  const urlYandex = {
+    'sport': ['https://news.yandex.ru/sport.html?from=rubric', '.story'],
+    'economic': ['https://news.yandex.ru/business.html?from=rubric', '.story'],
+    'politic': ['https://news.yandex.ru/politics.html?from=rubric', '.story']
+  };
+  const urlMailRu = {
+    'sport': [''],
+    'economic': [''],
+    'politic': ['']
+  };
+  switch (urlQuery.resource) {
+    case 'Yandex': {siteObj = urlYandex; break;}
+    case 'Mail.ru': {siteObj = urlMailRu; break;}
+  }
+  switch (urlQuery.theme){
+    case 'Спорт': {newsTheme = 'sport'; break;}
+    case 'Экономика': {newsTheme = 'economic'; break;}
+    case 'Политика': {newsTheme = 'politic'; break;}
+  }
   let html = renderIndex();
   const $ = cheerio.load(html);
+
   function yield(data) {
     $('#container').html(data);
     res.send($.html());
   }
-  sendRequest('https://news.yandex.ru/politics.html?from=rubric', '.story', yield);
+  sendRequest(siteObj[newsTheme][0], siteObj[newsTheme][1], yield);
 
 });
 
